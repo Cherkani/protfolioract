35e6e6ee553c24d9034472903b46538c5762a41f
Cherkani <cherkaniaymen1@gmail.com>
2023-05-19 05:02:57 +0100

    jj

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,89 +1,70 @@
 import React, { useState, useEffect } from "react";
-
-
-// import Logo from "../components/common/logo";
 import Footer from "../components/common/footer";
 import NavBar from "../components/common/navBar";
-
 import INFO from "../data/user";
-
-
 import "./styles/homepage.css";
 
 const Homepage = () => {
-	
-	const [logoSize, setLogoSize] = useState(80);
-	const [oldLogoSize, setOldLogoSize] = useState(80);
+  const [logoSize, setLogoSize] = useState(80);
+  const [oldLogoSize, setOldLogoSize] = useState(80);
 
-	useEffect(() => {
-		document.title = INFO.main.title;
-		window.scrollTo(0, 0);
-	}, []);
+  useEffect(() => {
+    document.title = INFO.main.title;
+    window.scrollTo(0, 0);
+  }, []);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			let scroll = Math.round(window.pageYOffset, 2);
+  useEffect(() => {
+    const handleScroll = () => {
+      let scroll = Math.round(window.pageYOffset, 2);
+      let newLogoSize = 80 - (scroll * 4) / 10;
 
-			let newLogoSize = 80 - (scroll * 4) / 10;
+      if (newLogoSize < oldLogoSize) {
+        if (newLogoSize > 40) {
+          setLogoSize(newLogoSize);
+          setOldLogoSize(newLogoSize);
+        }
+      } else {
+        setLogoSize(newLogoSize);
+      }
+    };
 
-			if (newLogoSize < oldLogoSize) {
-				if (newLogoSize > 40) {
-					setLogoSize(newLogoSize);
-					setOldLogoSize(newLogoSize);
-					
-				} 
-			} else {
-				setLogoSize(newLogoSize);
-				
-			}
-		};
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [logoSize, oldLogoSize]);
 
-		window.addEventListener("scroll", handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, [logoSize, oldLogoSize]);
-
-	
-
-	return (
-		<React.Fragment>
-			<div className="page-content">
-				<NavBar active="home" />
-				<div className="content-wrapper">
-					
-
-					<div className="homepage-container">
-						<div className="homepage-first-area">
-								<div className="homepage-first-area-left-side">
-          <div className="title homepage-title">{INFO.homepage.title}</div>
-
-          <div className="subtitle homepage-subtitle">
-            {INFO.homepage.description}
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <NavBar active="home" />
+        <div className="content-wrapper">
+          <div className="homepage-container">
+            <div className="homepage-first-area">
+              <div className="homepage-first-area-right-side">
+                <div className="homepage-image-container">
+                  <div className="homepage-image-wrapper">
+                    <img
+                      src="aymen profil.jpg"
+                      alt="about"
+                      className="homepage-image"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="homepage-first-area-left-side">
+                <div className="title homepage-title">{INFO.homepage.title}</div>
+                <div className="subtitle homepage-subtitle">
+                  {INFO.homepage.description}
+                </div>
+              </div>
+            </div>
+            <div className="page-footer">
+              <Footer />
+            </div>
           </div>
         </div>
-
-							<div className="homepage-first-area-right-side">
-								<div className="homepage-image-container">
-									<div className="homepage-image-wrapper">
-										<img
-											src="aymen profil.jpg"
-											alt="about"
-											className="homepage-image"
-										/>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						
-
-						<div className="page-footer">
-							<Footer />
-						</div>
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Homepage;
